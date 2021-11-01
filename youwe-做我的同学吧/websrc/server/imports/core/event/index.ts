530daa7d1d8135f542f2d67c89d8a6b1ecb1d61b
yuhj <snowwolfjay@foxmail.com>
2021-11-01 19:19:53 +0800

    WZX-1 忽略module和更新错误依赖

--- youwe-做我的同学吧/websrc/server/imports/core/event/index.ts
+++ youwe-做我的同学吧/websrc/server/imports/core/event/index.ts
@@ -1,10 +1,10 @@
-import { EventEmiter } from "shared/core";
+import { EventEmiter } from "./EventEmiter";
 
 export * from "./types";
 export * from "./EventEmiter";
-export const conBus = function<T = any>(
-    name: string,
-    options: { interval?: number; local?: boolean } = {}
-  ) {
-    return new EventEmiter(name)
-  };
+export const conBus = function <T = any>(
+  name: string,
+  options: { interval?: number; local?: boolean } = {}
+) {
+  return new EventEmiter(name)
+};
